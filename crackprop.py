59bf583673d50f4027096a94116ec2d267b48729
d-spoljaric <95293587+d-spoljaric@users.noreply.github.com>
2021-12-04 20:03:41 +0100

    class damage tolerance added

--- crackprop.py
+++ crackprop.py
@@ -1,1 +1,11 @@
-#this is the crack propagation file
+import Buckling as Bk
+import numpy as np
+
+class DamageTolerance:
+    def __init__(self, K_IC, K_I, a = 5*10**(-3)):
+        self.K_IC = K_IC
+        self.K_I = K_I
+        self.a = a
+
+    def stress_allowed(self):
+        return self.K_I/np.sqrt(np.pi*self.a)
