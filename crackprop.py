2e852ed35b58dc429fe22e20f001243c042e2682
Branco van Rijn <brancovanrijn@Brancos-MacBook-Pro.local>
2021-12-09 14:49:04 +0100

    updated crackprop

--- crackprop.py
+++ crackprop.py
@@ -13,15 +13,12 @@ class DamageTolerance:
 #Bk.BuckleWeb.total_shear
 def half_crack_length(stress_applied,KI): #KI is crack thoughness -> material property KI=29*10**6
     crack_length = ((KI/stress_applied)**2)/np.pi
-    total_crack_length = a*2
+    total_crack_length = crack_length*2
+    return total_crack_length
 
 def allowed_stress(crack_length, KI):
     max_allowed_tension_stress = KI/((np.pi*crack_length)**(1/2))
     return max_allowed_tension_stress
 
-
-
-
-
-#def stress_crack_tip():
-
+max_allowed_stress = round(allowed_stress(0.005,29*10**6)/10**6,2)
+print(max_allowed_stress, "MPa/M^(1/2)")
